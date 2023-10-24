60ef361cfc732755bc1c7152b9f385dc4f8edf41
Harry Dwarpen <harrydarkking@gmail.com>
2023-10-24 13:08:45 -0700

    feat/Add states and useStates to Sign Up Form

--- src/components/view/SignUp/index.tsx
+++ src/components/view/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Ads,
@@ -17,10 +17,26 @@ import { Link } from 'react-router-dom';
 import { PATH } from 'consts';
 
 export const SignupView: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+  const [code, setCode] = useState('');
+  const [username, setUsername] = useState('');
+  const [role, setRole] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(email);
+    console.log(password);
+    console.log(phone);
+    console.log(code);
+    console.log(username);
+    console.log(role);
+  };
   return (
     <Container>
       <Ads src={imgBack} />
-      <FormContainer>
+      <FormContainer onSubmit={handleSubmit}>
         <LogoUploader src="#" />
         <Title>Create an Account</Title>
         <div>
@@ -30,29 +46,53 @@ export const SignupView: React.FC = () => {
         <InputSection>
           <InputGroup>
             <InputTitle>Email Address</InputTitle>
-            <InputItem type="email" />
+            <InputItem
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </InputGroup>
           <InputGroup>
             <InputTitle>Password</InputTitle>
-            <InputItem type="password" />
+            <InputItem
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </InputGroup>
           <InputRowGroup>
             <InputGroup>
               <InputTitle>Phone Number</InputTitle>
-              <InputItem type="text" />
+              <InputItem
+                type="text"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
             </InputGroup>
             <InputGroup>
               <InputTitle>Country code</InputTitle>
-              <InputItem type="number" />
+              <InputItem
+                type="number"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
             </InputGroup>
           </InputRowGroup>
           <InputGroup>
             <InputTitle>Display Name</InputTitle>
-            <InputItem type="text" />
+            <InputItem
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </InputGroup>
           <InputGroup>
             <InputTitle>Role</InputTitle>
-            <InputItem type="text" />
+            <InputItem
+              type="text"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            />
           </InputGroup>
         </InputSection>
         <Submission type="submit">Submit</Submission>
