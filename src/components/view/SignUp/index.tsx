838186a7aa94712095eab5d6451e2216486baf65
Harry Dwarpen <harrydarkking@gmail.com>
2023-10-24 12:54:02 -0700

    feat/Initialize the layout and components of sign up page

--- src/components/view/SignUp/index.tsx
+++ src/components/view/SignUp/index.tsx
@@ -1,12 +1,62 @@
 import React from 'react';
-import { Container, Ads, FormContainer } from './styles';
+import {
+  Container,
+  Ads,
+  FormContainer,
+  LogoUploader,
+  Title,
+  InputGroup,
+  InputRowGroup,
+  InputItem,
+  InputTitle,
+  InputSection,
+  Submission,
+} from './styles';
 import imgBack from 'assets/imgs/imgBack.jpg';
+import { Link } from 'react-router-dom';
+import { PATH } from 'consts';
 
 export const SignupView: React.FC = () => {
   return (
     <Container>
       <Ads src={imgBack} />
-      <FormContainer></FormContainer>
+      <FormContainer>
+        <LogoUploader src="#" />
+        <Title>Create an Account</Title>
+        <div>
+          Already have an account?
+          <Link to={PATH.NOT_FOUND}>Log in</Link>
+        </div>
+        <InputSection>
+          <InputGroup>
+            <InputTitle>Email Address</InputTitle>
+            <InputItem type="email" />
+          </InputGroup>
+          <InputGroup>
+            <InputTitle>Password</InputTitle>
+            <InputItem type="password" />
+          </InputGroup>
+          <InputRowGroup>
+            <InputGroup>
+              <InputTitle>Phone Number</InputTitle>
+              <InputItem type="text" />
+            </InputGroup>
+            <InputGroup>
+              <InputTitle>Country code</InputTitle>
+              <InputItem type="number" />
+            </InputGroup>
+          </InputRowGroup>
+          <InputGroup>
+            <InputTitle>Display Name</InputTitle>
+            <InputItem type="text" />
+          </InputGroup>
+          <InputGroup>
+            <InputTitle>Role</InputTitle>
+            <InputItem type="text" />
+          </InputGroup>
+        </InputSection>
+        <Submission type="submit">Submit</Submission>
+      </FormContainer>
     </Container>
   );
 };
--- src/components/view/SignUp/styles.tsx
+++ src/components/view/SignUp/styles.tsx
@@ -32,3 +32,80 @@ export const FormContainer = styled.form`
   gap: 10px;
   align-items: center;
 `;
+
+export const LogoUploader = styled.img`
+  width: 72px;
+  height: 72px;
+  border-radius: 50%;
+  background-color: darkgray;
+`;
+
+export const Title = styled.p`
+  font-size: 1.5em;
+  font-weight: 600;
+  margin: 0;
+`;
+
+export const InputSection = styled.div`
+  width: 100%;
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const InputGroup = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 2px;
+`;
+
+export const InputRowGroup = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 70% auto;
+  gap: 10px;
+`;
+
+export const InputTitle = styled.div`
+  color: darkgray;
+`;
+
+export const InputItem = styled.input`
+  width: 100%;
+  height: 48px;
+  border-radius: 12px;
+  border: 1px solid gray;
+  outline: none;
+  padding: 0px 12px;
+  &:focus {
+    border: 1px solid darkgray;
+  }
+`;
+
+export const Submission = styled.button`
+  width: 100%;
+  height: 48px;
+  border-radius: 12px;
+  border: none;
+  outline: none;
+  text-align: center;
+  margin-top: 20px;
+  background: rgb(255, 154, 154);
+  background: linear-gradient(
+    55deg,
+    rgba(255, 154, 154, 1) 0%,
+    rgba(255, 108, 200, 1) 35%,
+    rgba(255, 151, 174, 1) 100%
+  );
+  color: white;
+  transition: 0.5s;
+  &:hover {
+    cursor: pointer;
+    background: rgb(185, 185, 185);
+    box-shadow: 1px 1px 4px rgb(0.2, 0.2, 0.2, 0.4);
+  }
+`;
